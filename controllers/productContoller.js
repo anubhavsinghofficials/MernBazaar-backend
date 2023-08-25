ba87394e313ef837ac634a07b1ebd55d74eb7a24
Anubhav Singh <anubhavsinghdelhi23@gmail.com>
2023-08-25 14:20:23 +0530

    Added Price range filter function

--- controllers/productContoller.js
+++ controllers/productContoller.js
@@ -4,8 +4,6 @@ import Product from '../models/productModel.js'
 
 
 // _____________________________________ADMIN ROUTES
-//--------------------------------------------------
-
 
 
 export const createProduct = async (req,res) => {
@@ -85,22 +83,33 @@ export const deleteProduct = async (req,res) => {
 
 
 // ______________________________________USER ROUTES
-//--------------------------------------------------
 
 
 export const getProducts = async (req,res) => {
     try {
-        const {keyword,pageNo,pageLength} = req.query
-        let products;
+        const {keyword,category,price,pageNo,pageLength} = req.query
 
-        const searchFilter = keyword ? { autoTags: {$regex:keyword, $options:'i'} }
-                                     : {};
+        let filter = keyword ? {autoTags:{$regex:keyword,$options:'i'}}:{}
+            filter = category ? {...filter, category}:{...filter}
+            
+        if (price) {
+            // this price is in the form of {gt:'0.1'}, but for mongoose
+            // we should have {'$gt':0.1}, thats what we are doing here
+            const regex = /\b(gt|gte|lt|lte)\b/g
+            const netPriceString = JSON.stringify(price)
+                                       .replace(regex, key => `$${key}`)
+            const netPrice = JSON.parse(netPriceString)
+            Object.keys(netPrice).forEach(key => netPrice[key] = +netPrice[key])
+            filter = {...filter, "price.net":netPrice}
+        }
 
-        products = await Product.find(searchFilter)
-                                .skip((+pageNo - 1)*(+pageLength))
-                                .limit(pageLength)
+        const products = await Product.find(filter)
+                                      .limit(pageLength)
+                                      .skip((+pageNo - 1)*(+pageLength))
 
-        res.status(200).json(products)
+        const totalProducts = await Product.countDocuments(filter)
+
+        res.status(200).json({totalProducts,products})
 
     } catch (error) {
         res.status(400).json({error:error.message})
