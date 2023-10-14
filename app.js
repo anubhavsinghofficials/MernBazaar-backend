9ec3dca8ebdf8c89b01e016d5425f12e7e812be3
Anubhav Singh <anubhavsinghdelhi23@gmail.com>
2023-10-14 15:40:23 +0530

    fix: allow credentials

--- app.js
+++ app.js
@@ -20,7 +20,7 @@ const app = express()
 
 connectToDatabase()
 
-app.use(cors({origin:ClientOrigin}))
+app.use(cors({origin:ClientOrigin, credentials: true,}))
 app.use(cookieParser())
 app.use(express.json())
 app.use("/api/v1",productRouter)
